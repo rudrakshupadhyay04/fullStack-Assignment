08cce3cf253b595b60b178c53675bb28ae079615
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-08-18 21:42:38 +0530

    create a card model

--- backEnd/index.js
+++ backEnd/index.js
@@ -12,6 +12,14 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 }).catch((error)=>{console.log(error)});
 
 
+// basic server to check
+app.get('/ping' , (req,res) => {
+    res.json({
+        message:"Server is running fine!!!"
+    })
+})
+
+
 app.listen(process.env.PORT, (req, res) => {
     console.log("server running at port 3000");
 })
