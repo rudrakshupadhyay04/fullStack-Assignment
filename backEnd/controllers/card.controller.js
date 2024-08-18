7cb56fed94c7d9ff29e24dbd3e0abe9f3420dca7
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-08-18 23:12:37 +0530

    getParticularCard api completed

--- backEnd/controllers/card.controller.js
+++ backEnd/controllers/card.controller.js
@@ -29,9 +29,26 @@ export const createCard = async (req,res) => {
 export const getAllCards = async (req,res) => {
     try {
         const cards = await Card.find();
-        console.log(cards);
+        // console.log(cards);
         return res.status(200).json(cards);
     } catch (error) {
         console.log(error);
     }
 };
+
+export const getParticularCard = async (req,res) => {
+    try {
+        const card = await Card.findOne({title: req.params.title});
+        if(!card){
+            return res.status(400).json({
+                message:"Card not found!!!",
+                success:false
+            })
+        } 
+
+        return res.status(200).json(card);
+        
+    } catch (error) {
+        console.log(first)
+    }
+}
--- backEnd/routes/card.routes.js
+++ backEnd/routes/card.routes.js
@@ -1,9 +1,10 @@
 import express from'express';
-import { createCard, getAllCards } from '../controllers/card.controller.js';
+import { createCard, getAllCards, getParticularCard } from '../controllers/card.controller.js';
 
 const router = express.Router();
 
 router.post('/cards', createCard);
 router.get('/cards', getAllCards);
+router.get('/cards/:title', getParticularCard);
 
 export default router;
